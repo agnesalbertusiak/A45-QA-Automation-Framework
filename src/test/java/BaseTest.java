96ec0f634a0d0fb6588d6b3ef5c4cd36737dfec6
Agnes A <agnes.albertusiak@testpro.io>
2023-05-02 17:52:35 -0500

    Homework21

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -17,6 +18,9 @@ import java.time.Duration;
 public class BaseTest {
     WebDriver driver;
     WebDriverWait wait;
+
+    Actions actions = null;
+
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
@@ -29,7 +33,7 @@ public class BaseTest {
 
         driver = new ChromeDriver(options);
         wait = new WebDriverWait(driver, Duration.ofSeconds(4));
-
+        actions = new Actions(driver);
     }
     @AfterMethod
     public void closeBrowser(){
@@ -55,33 +59,33 @@ public class BaseTest {
         passwordField.sendKeys(te$tStudent1);
     }
 
-    public void clickSubmit() throws InterruptedException{
+    public void clickSubmit(){
         WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[type='submit']")));
         submitButton.click();
 
 
     }
 
-    public void searchSong(String epicSong) throws InterruptedException{
+    public void searchSong(String epicSong) {
         WebElement searchField = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[name='q']")));
         searchField.sendKeys(epicSong);
 
     }
-    public void clickViewAllBtn() throws InterruptedException{
+    public void clickViewAllBtn() {
         WebElement viewAllSearch = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("div.results section.songs h1 button")));
         viewAllSearch.click();
     }
-    public void selectFirstSong() throws InterruptedException{
+    public void selectFirstSong() {
         WebElement selectSong = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("section#songResultsWrapper tr.song-item td.title")));
         selectSong.click();
     }
 
-    public void clickAddToBtn() throws InterruptedException{
+    public void clickAddToBtn(){
         WebElement addToBtn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button.btn-add-to")));
         addToBtn.click();
     }
 
-    public void choosePlaylist() throws InterruptedException{
+    public void choosePlaylist() {
         WebElement chooseAnyPlaylist = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//section[@id='songResultsWrapper']//li[contains(text(),'yes')]")));
         chooseAnyPlaylist.click();
     }
